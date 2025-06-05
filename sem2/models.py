8e4b0acdff9e67abca4b73b3c4bf97a34f30dd5d
Nikolai Braginets <140839023+nikolai-kolia@users.noreply.github.com>
2025-06-05 09:42:24 +0300

    Delete sem2 directory

--- sem2/models.py
+++
@@ -1,8 +0,0 @@
-# project/app/db/models.py
-from sqlalchemy.ext.declarative import declarative_base
-
-# Базовый класс для моделей SQLAlchemy
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
